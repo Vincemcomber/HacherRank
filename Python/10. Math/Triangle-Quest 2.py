10af14525de23084ab0ccd64430b23172c5ea726
Vincent Mukomba <124879172+Vincemcomber@users.noreply.github.com>
2024-11-21 17:09:56 +0200

    Add files via upload

---
+++ Python/10. Math/Triangle-Quest 2.py
@@ -0,0 +1,15 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/triangle-quest-2/problem?isFullScreen=true
+
+# Language: Python
+
+# ========================
+#         Solution
+# ========================
+
+for i in range(1,int(input())+1): 
+    print(((10**i-1)//9)**2)
+    
