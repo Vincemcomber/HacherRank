8e6756b4614850d5347bd6e904690dc7de15724d
Vincent Mukomba <124879172+Vincemcomber@users.noreply.github.com>
2024-12-26 16:43:31 +0200

    Add files via upload

---
+++ Algorithms/2. Implementation/33-Cut the sticks.py
@@ -0,0 +1,49 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/cut-the-sticks/problem?isFullScreen=true
+
+
+# Language: Python
+
+# ========================
+#         Solution
+# ========================
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'cutTheSticks' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def cutTheSticks(arr):
+    lst=[]
+    count=[]
+    while (len(arr)!=0):
+        count.append(len(arr))
+        arr=[x-min(arr) for x in arr if (x-min(arr)!=0)]
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = cutTheSticks(arr)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
