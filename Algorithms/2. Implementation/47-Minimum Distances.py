4e0bf6d26e52c9aacc9e17090a6b5ad03590c469
Vincent Mukomba <124879172+Vincemcomber@users.noreply.github.com>
2025-01-15 13:24:57 +0200

    Add files via upload

---
+++ Algorithms/2. Implementation/47-Minimum Distances.py
@@ -0,0 +1,45 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/minimum-distances/problem?isFullScreen=true
+
+
+# Language: Python
+
+# ========================
+#         Solution
+# ========================
+
+
+import sys
+
+
+def minimumDistances(a):
+
+    b = [(v, i) for i, v in enumerate(a)]
+    b = sorted(b)
+
+    print('a=',a, file=sys.stderr)
+    print('b=', b, file=sys.stderr)
+
+    iprec = 0
+    vprec = -1
+    m = len(a)
+
+    for v, i in b:
+        if v == vprec:
+            m = min(m, i - iprec)
+        iprec = i
+        vprec = v
+
+    if m == len(a):
+        m = -1
+    return m
+
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    a = list(map(int, input().strip().split(' ')))
+    result = minimumDistances(a)
+    print(result)
