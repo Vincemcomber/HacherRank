082a812fe16a2f428cb20d0ec2927f3f8becf519
Vincent Mukomba <124879172+Vincemcomber@users.noreply.github.com>
2025-01-17 19:03:19 +0200

    Add files via upload

---
+++ Algorithms/2. Implementation/50-Chocolate Feast.py
@@ -0,0 +1,35 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/chocolate-feast/problem?isFullScreen=true
+
+
+# Language: Python
+
+# ========================
+#         Solution
+# ========================
+
+#!/bin/python3
+
+import sys
+
+def wrappers(wr, m):
+    res = 0
+    if wr//m > 0:
+        res += wr//m + wrappers(wr//m + wr%m, m)
+    return res
+    
+
+def chocolateFeast(n, c, m):
+    return n//c + wrappers(n//c, m)
+
+
+if __name__ == "__main__":
+    t = int(input().strip())
+    for a0 in range(t):
+        n, c, m = input().strip().split(' ')
+        n, c, m = [int(n), int(c), int(m)]
+        result = chocolateFeast(n, c, m)
+        print(result)
