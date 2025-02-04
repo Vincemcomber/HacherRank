3283514a375e4ba2df321987a1c5f4207b2387a3
Vincent Mukomba <124879172+Vincemcomber@users.noreply.github.com>
2025-02-04 12:54:42 +0200

    Add files via upload

---
+++ SQL/1. Basic Select/10. Weather Observation Station 5.sql
@@ -0,0 +1,29 @@
+/*
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+The STATION table is described as follows:
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
+*/
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+SELECT CITY, LENGTH(CITY) AS LENGTH
+FROM STATION
+ORDER BY LENGTH ASC, CITY
+LIMIT 1;
+
+SELECT CITY, LENGTH(CITY) AS LENGTH
+FROM STATION
+ORDER BY LENGTH DESC, CITY
+LIMIT 1;
---
+++ SQL/1. Basic Select/9. Weather Observation Station 4.sql
@@ -0,0 +1,25 @@
+/*
+Weather Observation Station 4
+=============================
+Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+The STATION table is described as follows:
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMBER     |
+| LONG_W      | NUMBER     |
++-------------+------------+
+
+*/
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
