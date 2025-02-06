5f6a493667ea89c7f2bdb66647da09b5918ceb08
Vincent Mukomba <124879172+Vincemcomber@users.noreply.github.com>
2025-02-06 14:57:50 +0200

    Add files via upload

---
+++ SQL/1. Basic Select/12. Weather Observation Station 7.sql
@@ -0,0 +1,25 @@
+/*
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMBER     |
+| LONG_W      | NUMBER     |
++-------------+------------+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+SELECT  DISTINCT CITY FROM STATION
+WHERE  RIGHT(UPPER(CITY),1)  IN('A','E','I','O','U');
